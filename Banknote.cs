817040582e83e2742f97f9fba42f26b0167b5915
hopheylalaley <mickevich.ja@gmail.com>
2015-03-22 21:59:45 +0300

    Добавил класс Banknote

---
+++ Banknote.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace ATM
+{
+    public class Banknote
+    {
+        public decimal Nominal { get; set; }
+
+        public Banknote(decimal nominal)
+        {
+            Nominal = nominal;
+        }
+
+        public override string ToString()
+        {
+            return Nominal.ToString(CultureInfo.CurrentCulture);
+        }
+    }
+}
